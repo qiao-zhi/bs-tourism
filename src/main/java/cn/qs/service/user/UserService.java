2f2aafa1169a2489ff6cf505df312fca813a3f86
qiaozhi <qiao_liqiang@163.com>
2019-03-26 21:35:03 +0800

    init

---
+++ src/main/java/cn/qs/service/user/UserService.java
@@ -0,0 +1,27 @@
+package cn.qs.service.user;
+
+
+
+import java.util.List;
+import java.util.Map;
+
+import cn.qs.bean.user.User;
+
+
+public interface UserService {
+	
+	/**
+	 * 根据接口查询所用的用户
+	 */
+	public List<User> findAllUser();
+
+	public void addUser(User user);
+
+	public List<User> getUsers(Map condition);
+
+	public void deleteUser(int id);
+
+	public User getUser(int id);
+
+	public void updateUser(User user);
+}
